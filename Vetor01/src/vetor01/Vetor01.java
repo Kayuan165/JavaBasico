b6436538a01acd9cde3844aa7f1f96916b49445f
Kayuan Dias Oliveira <108407043+Kayuan165@users.noreply.github.com>
2024-04-29 22:50:24 -0300

    Add files via upload

---
+++ Vetor01/src/vetor01/Vetor01.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package vetor01;
+
+/**
+ *
+ * @author Kayuan
+ */
+public class Vetor01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        int n[] = {1,2,8,4,5,6};;
+        
+        for(int c=0;c <= n.length-1; c++){
+            System.out.print(n[c]);
+            
+        }
+    }
+    
+}
