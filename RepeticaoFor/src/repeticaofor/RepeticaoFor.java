dd8c99f68e75ad8451457c7bb0d00831260356a3
Kayuan Dias Oliveira <108407043+Kayuan165@users.noreply.github.com>
2024-04-29 22:47:06 -0300

    Add files via upload

---
+++ RepeticaoFor/src/repeticaofor/RepeticaoFor.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package repeticaofor;
+
+/**
+ *
+ * @author Kayuan
+ */
+public class RepeticaoFor {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        int cc;
+        
+        for( cc = 0; cc <=100; cc+=10){
+            System.out.println(cc);
+        }
+    }
+    
+}
